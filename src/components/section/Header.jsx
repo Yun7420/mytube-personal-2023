1744e646a2957761a0efc01f28b9c2dc2475ba2b
hsy7420 <hsy742010@gmail.com>
2023-10-30 15:37:56 +0900

    07 헤더 영역 완성하기

--- src/components/section/Header.jsx
+++ src/components/section/Header.jsx
@@ -17,13 +17,13 @@ import { AiOutlineInstagram } from "react-icons/ai";
 const Header = () => {
   return (
     <header id='header' role='banner'>
-        <h1 className='header_logo'>
+        <h1 className='header__logo'>
             <a href='/'>
                 <em aria-hidden='true'></em>
                 <span>webs<br />youtube</span>
             </a>
         </h1>
-        <nav className='header_menu'>
+        <nav className='header__menu'>
             <ul className='menu'>
                 <li className='active'>
                     <a href='/'>
@@ -67,43 +67,64 @@ const Header = () => {
                 </li>
             </ul>
             <ul className='keyword'>
-                    <li>
-                        <a href='/search/webstoryboy'>webstoryboy</a>
-                    </li>
-                    <li>
-                        <a href='/search/html'>HTML</a>
-                    </li>
-                    <li>
-                        <a href='/search/css'>CSS</a>
-                    </li>
-                    <li>
-                        <a href='/search/javascript'>JavaScript</a>
-                    </li>
-                    <li>
-                        <a href='/search/react.js'>React.js</a>
-                    </li>
-                    <li>
-                        <a href='/search/vue.js'>Vue.js</a>
-                    </li>
-                    <li>
-                        <a href='/search/next.js'>Next.js</a>
-                    </li>
-                    <li>
-                        <a href='/search/node.js'>Node.js</a>
-                    </li>
-                    <li>
-                        <a href='/search/sql'>SQL</a>
-                    </li>
-                    <li>
-                        <a href='/search/React Portfolio'>portfolio</a>
-                    </li>
-                    <li>
-                        <a href='/search/NewJeans'>music</a>
-                    </li>
+                <li className='active'>
+                    <a href='/search/webstoryboy'>webstoryboy</a>
+                </li>
+                <li>
+                    <a href='/search/html'>HTML</a>
+                </li>
+                <li>
+                    <a href='/search/css'>CSS</a>
+                </li>
+                <li>
+                    <a href='/search/javascript'>JavaScript</a>
+                </li>
+                <li>
+                    <a href='/search/react.js'>React.js</a>
+                </li>
+                <li>
+                    <a href='/search/vue.js'>Vue.js</a>
+                </li>
+                <li>
+                    <a href='/search/next.js'>Next.js</a>
+                </li>
+                <li>
+                    <a href='/search/node.js'>Node.js</a>
+                </li>
+                <li>
+                    <a href='/search/sql'>SQL</a>
+                </li>
+                <li>
+                    <a href='/search/React Portfolio'>portfolio</a>
+                </li>
+                <li>
+                    <a href='/search/NewJeans'>music</a>
+                </li>
             </ul>
         </nav>
-        <div className='header_sns'>
-
+        <div className='header__sns'>
+            <ul>
+                <li>
+                    <a href='https://github.com/webstoryboy' rel='noopener noreferrer'>
+                        <AiFillGithub />
+                    </a>
+                </li>
+                <li>
+                    <a href='https://www.youtube.com/webstoryboy' rel='noopener noreferrer'>
+                        <AiFillYoutube />
+                    </a>
+                </li>
+                <li>
+                    <a href='https://codepen.io/webstoryboy' rel='noopener noreferrer'>
+                        <AiOutlineCodepen />
+                    </a>
+                </li>
+                <li>
+                    <a href='https://www.instagram.com/webstoryboy' rel='noopener noreferrer'>
+                        <AiOutlineInstagram />
+                    </a>
+                </li>
+            </ul>
         </div>
     </header>
   )
